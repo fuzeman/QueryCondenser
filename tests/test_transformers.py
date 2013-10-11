fb0e8262f0127fddfb3682712e13c4ce7d17c593
Dean Gardiner <gardiner91@gmail.com>
2013-10-12 01:05:02 +1300

    Added some simple tests for MergeTransformer.run and SliceTransformer.run

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from qcond import MergeTransformer, SliceTransformer
 from qcond.transformers.base import Transformer
 
 
@@ -9,13 +10,48 @@ class TestTransformer(TestCase):
 
 
 class TestMergeTransformer(TestCase):
+    def setUp(self):
+        self.merge = MergeTransformer()
+
     def test_run(self):
-        self.fail()
+        self.assertSequenceEqual(self.merge.run([
+            "Don't Trust the B---- in Apartment 23",
+            "Apartment 23",
+            "Apt 23",
+            "Don't Trust the B in Apt 23",
+            "Don't Trust the B- in Apt 23",
+            "Don't Trust the Bitch in Apartment 23",
+            "Don't Trust the Bitch in Apt 23",
+            "Dont Trust the Bitch in Apartment 23"
+        ]), [
+            'dont trust the',
+            'dont trust the apt 23',
+            'dont trust the apartment 23',
+            'apt 23',
+            'apartment 23'
+        ])
 
     def test_merge(self):
-        self.fail()
+        pass
 
 
 class TestSliceTransformer(TestCase):
+    def setUp(self):
+        self.slice = SliceTransformer()
+
     def test_run(self):
-        self.fail()
+        self.assertSequenceEqual(self.slice.run([
+            "Don't Trust the B---- in Apartment 23",
+            "Apartment 23",
+            "Apt 23",
+            "Don't Trust the B in Apt 23",
+            "Don't Trust the B- in Apt 23",
+            "Don't Trust the Bitch in Apartment 23",
+            "Don't Trust the Bitch in Apt 23",
+            "Dont Trust the Bitch in Apartment 23"
+        ]), [
+            'Apartment 23',
+            'Apt 23',
+            "Don't Trust the B in Apt 23",
+            'Dont Trust the Bitch in Apartment 23'
+        ])
